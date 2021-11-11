9df922e280c4eba16ea3c46733f52668b0bdf7cd
jailton.junior <jailton.junior@iterative.com.br>
2021-11-11 12:13:50 -0300

    removendo informações sensiveis

--- src/infra/output.tf
+++ src/infra/output.tf
@@ -2,5 +2,3 @@ output "postgresql_connectionstring" {
   description = "Connection string for the Azure PostgreSql"
   value       = "postgres://${azurerm_postgresql_server.botpress_postgresql.administrator_login}:${nonsensitive(azurerm_postgresql_server.botpress_postgresql.administrator_login_password)}@${azurerm_postgresql_server.botpress_postgresql.fqdn}:5432/${azurerm_postgresql_database.botpress_database.name}?ssl=true"
 }
-
-# postgres://conectcar@qa-pg-botpress:@conect@2021@qa-pg-botpress.postgres.database.azure.com:5432/botpress_db?ssl=true
